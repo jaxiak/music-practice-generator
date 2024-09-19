a53675ed40b8f2b68aa76fe46f5b4839744989c5
jaxiak <561523+jaxiak@users.noreply.github.com>
2024-09-19 10:04:33 -0400

    Cleaned git tracking, toml, and updated test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,5 @@
 # import lilypad
+
 from .MusicPracticeGenerator  import write_random_chords
 
 import random
@@ -11,6 +12,8 @@ write_random_chords()
 test_reader = open('output/sample_rythms.ly')
 # test_output = 
 
+def test_rhythm_output():
+    assert True
 # assert test_output == example_output
 
 
